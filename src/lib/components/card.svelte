18bdd9054144936046f87055ccfe97d77969c63a
Christine Fredricksen <cmfredricksen@gmail.com>
2021-12-16 20:51:38 -0600

    added card component

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,7 @@
+<div class="card">
+	<img src="../static/images" alt="Icon for site" />
+	<h2 class="title">
+		<a href="https://technotes.cmfred.com">Tech Notes</a>
+	</h2>
+	<p>Simple Documentation Website for beginner's with Git & GitHub</p>
+</div>
