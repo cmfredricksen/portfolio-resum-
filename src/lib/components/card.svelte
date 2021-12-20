806b27bdf43ac81175e2157d6781ab553a6d06bd
Christine Fredricksen <cmfredricksen@gmail.com>
2021-12-20 15:45:10 -0600

    added png for one card

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,7 +1,14 @@
+sc
 <div class="card">
-	<img src="../static/images" alt="Icon for site" />
+	<img src="../static/026-coding.png" alt="Icon for site" />
 	<h2 class="title">
 		<a href="https://technotes.cmfred.com">Tech Notes</a>
 	</h2>
 	<p>Simple Documentation Website for beginner's with Git & GitHub</p>
 </div>
+
+<style>
+	.card {
+		border: 2px solid lightblue;
+	}
+</style>
