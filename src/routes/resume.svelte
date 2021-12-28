3edcb82a894c6b255f6e713839632fec76a1a46c
Christine Fredricksen <cmfredricksen@gmail.com>
2021-12-28 17:10:11 -0600

    added devicons & basic info to resumé page

---
+++ src/routes/resume.svelte
@@ -0,0 +1,68 @@
+<script>
+	import '../../static/global.css';
+</script>
+
+<svelte:head>
+	<title>Resumé of CMF</title>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css"
+	/>
+</svelte:head>
+
+<div class="title">
+	<h1>Chris Fredricksen</h1>
+	<h2>Resumé</h2>
+</div>
+
+<div class="info">
+	<ul>
+		<li>
+			<h3><a href="mailto:chris@cmfred.com">chris@cmfred.com</a></h3>
+		</li>
+		<li>
+			<h3>218-504-9155</h3>
+		</li>
+		<li>
+			<h3>Ely, Minnesota</h3>
+		</li>
+	</ul>
+</div>
+
+<div class="icons">
+	<i class="devicon-javascript-plain colored" />
+	<i class="devicon-html5-plain colored" />
+	<i class="devicon-css3-plain colored" />
+	<i class="devicon-sass-plain colored" />
+	<i class="devicon-nodejs-plain colored" />
+	<i class="devicon-express-plain colored" />
+	<i class="devicon-svelte-plain colored" />
+	<i class="devicon-mongodb-plain colored" />
+	<i class="devicon-apple-plain " />
+	<i class="devicon-jamstack-plain colored" />
+	<i class="devicon-bash-plain " />
+	<i class="devicon-vscode-plain colored" />
+	<i class="devicon-vim-plain colored" />
+	<i class="devicon-linux-plain colored" />
+	<i class="devicon-ubuntu-plain colored" />
+	<i class="devicon-chrome-plain colored" />
+	<i class="devicon-filezilla-plain colored" />
+	<i class="devicon-google-plain colored" />
+	<i class="devicon-git-plain colored" />
+	<i class="devicon-github-plain colored" />
+	<i class="devicon-markdown-plain colored" />
+	<i class="devicon-npm-plain colored" />
+	<i class="devicon-typescript-plain colored" />
+	<i class="devicon-wordpress-plain colored" />
+	<i class="devicon-woocommerce-plain colored" />
+</div>
+
+<style>
+	.icons {
+		padding: 2rem;
+		background-color: var(--clr-headings);
+	}
+	i {
+		font-size: 3rem;
+	}
+</style>
