67b3c3c6742f74c421af2ec93d6ea575969d9a90
Christine Fredricksen <cmfredricksen@gmail.com>
2021-12-28 20:19:21 -0600

    formatted icons, added color

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -30,39 +30,44 @@
 </div>
 
 <div class="icons">
-	<i class="devicon-javascript-plain colored" />
-	<i class="devicon-html5-plain colored" />
-	<i class="devicon-css3-plain colored" />
-	<i class="devicon-sass-plain colored" />
-	<i class="devicon-nodejs-plain colored" />
-	<i class="devicon-express-plain colored" />
-	<i class="devicon-svelte-plain colored" />
-	<i class="devicon-mongodb-plain colored" />
+	<i class="devicon-javascript-plain " />
+	<i class="devicon-html5-plain" />
+	<i class="devicon-css3-plain" />
+	<i class="devicon-sass-plain" />
+	<i class="devicon-nodejs-plain" />
+	<i class="devicon-express-original-wordmark" />
+	<i class="devicon-svelte-plain" />
+	<i class="devicon-mongodb-plain" />
 	<i class="devicon-apple-plain " />
-	<i class="devicon-jamstack-plain colored" />
+	<i class="devicon-jamstack-plain" />
 	<i class="devicon-bash-plain " />
-	<i class="devicon-vscode-plain colored" />
-	<i class="devicon-vim-plain colored" />
-	<i class="devicon-linux-plain colored" />
-	<i class="devicon-ubuntu-plain colored" />
-	<i class="devicon-chrome-plain colored" />
-	<i class="devicon-filezilla-plain colored" />
-	<i class="devicon-google-plain colored" />
-	<i class="devicon-git-plain colored" />
-	<i class="devicon-github-plain colored" />
-	<i class="devicon-markdown-plain colored" />
-	<i class="devicon-npm-plain colored" />
-	<i class="devicon-typescript-plain colored" />
-	<i class="devicon-wordpress-plain colored" />
-	<i class="devicon-woocommerce-plain colored" />
+	<i class="devicon-vscode-plain" />
+	<i class="devicon-vim-plain" />
+	<i class="devicon-linux-plain" />
+	<i class="devicon-ubuntu-plain" />
+	<i class="devicon-chrome-plain" />
+	<i class="devicon-filezilla-plain" />
+	<i class="devicon-google-plain" />
+	<i class="devicon-git-plain" />
+	<i class="devicon-github-plain" />
+	<i class="devicon-markdown-plain" />
+	<i class="devicon-npm-original-wordmark" />
+	<i class="devicon-typescript-plain" />
+	<i class="devicon-wordpress-plain" />
+	<i class="devicon-woocommerce-plain" />
 </div>
 
 <style>
 	.icons {
 		padding: 2rem;
-		background-color: var(--clr-headings);
+		/* background-color: var(--clr-secondary); */
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		width: 40vw;
 	}
 	i {
 		font-size: 3rem;
+		color: var(--clr-text);
+		margin: 1rem 0;
 	}
 </style>
